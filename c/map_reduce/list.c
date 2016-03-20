8b61080c76d6e073ecb0e9718ffaa98a3092a568
evgeniy <eveniy.klyuchikov@gmail.com>
2016-03-20 22:05:36 +0300

    C project fixed

--- c/map_reduce/list.c
+++ c/map_reduce/list.c
@@ -8,26 +8,35 @@ int list_init(struct list *list) {
 	list->head = (void **)malloc(sizeof(void *) * 4);
 	list->current_length = 0;
 	list->full_length = 4;
+    list->mutex = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t));
 	pthread_mutex_init(list->mutex, NULL);
 	return EXIT_SUCCESS;
 }
 
 int list_add(struct list *list, void *element) {
+    int i;
 	if(list->current_length == list->full_length) {
-		void **new_array = (void **)malloc(sizeof(void *) * 2 * list->current_length);
-		memcpy(new_array, list->head, list->current_length);
+        list->full_length *= 2;
+        void **new_array = (void **)malloc(sizeof(void *) * list->full_length);
+
+        for(i = 0; i < list->current_length; ++i)
+            new_array[i] = (list->head)[i];
+        //memcpy(new_array, list->head, list->current_length);
 		free(list->head);
 		list->head = new_array;
 	}	
 
 	*(list->head + list->current_length) = element;
+    ++(list->current_length);
 	return EXIT_SUCCESS;
 }
 
 
 int list_destroy(struct list *list) {
 	pthread_mutex_destroy(list->mutex);
+	free(list->mutex);
 	free(list->head);
-	//free(list);
+    //free(list);
+    return 0;
 }
 
--- c/map_reduce/main.c
+++ c/map_reduce/main.c
@@ -3,25 +3,47 @@
 #include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
 void *find_words(void*);
 
+int map_reduce(int number_threads, char *string, struct list *list);
+
 int main(void) {
-	struct list *list = (struct list *)malloc(sizeof(struct list));
-	list_init(list);
-	char *string = "abc 123, 135,,,,34,4!sdf";
-	void **arg = (void **)malloc(sizeof(void) * 3);
-	arg[0] = list;
-	arg[1] = string;
-	arg[2] = string + strlen(string) - 1;
-	map_reduce(1, string);
-	find_words((void*)arg);
+    char *string;
+    int cur_time, i, j;
+    struct list *list;
+    struct stat *buf;
 
-	int i;
-	for(i = 0; i < list->current_length; ++i) {
-		printf("%s\n", (char*)list->head[i]);
-	}
+    buf = (struct stat *) malloc(sizeof(struct stat));
+    stat("./Don_Quixote.txt", buf);
 
+    int fd = open("./Don_Quixote.txt", O_RDONLY);
+    string = (char *)malloc(sizeof(char) * buf->st_size);
+    read(fd, string, buf->st_size);
+
+    list = (struct list *)malloc(sizeof(struct list));
+    for(j = 2; j < 4; ++j) {
+		list_init(list);
+	
+		cur_time = clock();
+        map_reduce(j, string, list);
+		cur_time = clock() - cur_time;
+
+		printf("%d\n\n", cur_time);
+		//for(i = 0; i < list->current_length; ++i)
+		//	printf("%s\n", (char*)list->head[i]);
+
+		for(i = 0; i < list->current_length; ++i)
+			free(list->head[i]);
+		list_destroy(list);
+    }
+
+	free(string);
+    free(list);
+    exit(0);
 }
 
 int is_separator(char *simbol, char *separators) {
@@ -35,24 +57,24 @@ int is_separator(char *simbol, char *separators) {
 
 void *find_words(void *arg) {
 	struct list *list;
-	char *begin, *end;
-	char *new_begin, *new_str;
+    char *begin, *end;
+    char *new_begin, *new_str;
 	int i, is_unique;
 	void **pointer;
 
 	pointer = (void **)arg;
 
 	list = (struct list *)(pointer[0]);
-	begin = (char *)(pointer[1]);
-	end = (char *)(pointer[2]);
+    begin = (char *)(pointer[1]);
+    end = (char *)(pointer[2]);
 
-	while(begin != end) {
-		while(begin <= end && is_separator(begin, ",.?!"))
+    while(begin != end) {
+        while(begin <= end && is_separator(begin, " ,.?!"))
 			++begin;
 		if(begin > end)
 			return NULL;
 		new_begin = begin;
-		while(new_begin <= end && !is_separator(new_begin, ",.?!"))
+        while(new_begin <= end && !is_separator(new_begin, " ,.?!"))
 			++new_begin;
 
 		--new_begin;
@@ -69,25 +91,23 @@ void *find_words(void *arg) {
 		if(is_unique == 0) {
 			new_str = (char *)malloc(sizeof(char) * (new_begin - begin + 2));
 			memcpy(new_str, begin, new_begin - begin + 1);
-			new_str[new_begin - begin + 1] = '\0';
+            new_str[new_begin - begin + 1] = '\0';
 			list_add(list, new_str);
 		}
 		pthread_mutex_unlock(list->mutex);
+        usleep(1000);
+        begin = new_begin + 1;
 	}
-	free(arg);
-	return NULL;
+    pthread_exit(0);
 }
 
-int map_reduce(int number_threads, char *string) {
+int map_reduce(int number_threads, char *string, struct list *list) {
 	int length, delta_length, i, begin_offset, end_offset, cur_pthread;
-	struct list *list;
-	void **arg;
+	void ***arg;
 	pthread_t *pthreads;
 
-	list = (struct list*)malloc(sizeof(struct list));
-	list_init(list);
-
-	pthreads = (pthread_t *)malloc(sizeof(pthread_t)*number_threads);
+    pthreads = (pthread_t *)malloc(sizeof(pthread_t)*number_threads);
+	arg = (void ***)malloc(sizeof(void *) * number_threads);
    
 	length = strlen(string);
 	delta_length = length / number_threads;
@@ -98,28 +118,28 @@ int map_reduce(int number_threads, char *string) {
 		if(i == number_threads - 1)
 			end_offset = length;
 		else {
-			end_offset = delta_length * i;
-			while(end_offset >= begin_offset && !is_separator(string + end_offset, ",.?!"))
+            end_offset = delta_length * (i + 1);
+            while(end_offset >= begin_offset && !is_separator(string + end_offset, " ,.?!"))
 				--end_offset;
 		}
 		if(end_offset >= begin_offset) {
-			arg = (void **)malloc(sizeof(void) * 3);
-			arg[1] = list;
-			arg[2] = string + begin_offset;
-			arg[3] = string + end_offset;
-			pthread_create((pthreads + cur_pthread), NULL, find_words, (void*)arg);
+			arg[cur_pthread] = (void **)malloc(sizeof(void *) * 3);
+            arg[cur_pthread][0] = list;
+            arg[cur_pthread][1] = string + begin_offset;
+            arg[cur_pthread][2] = string + end_offset - 1;
+			pthread_create((pthreads + cur_pthread), NULL, find_words, (void*)(arg[cur_pthread]));
+            ++cur_pthread;
 		}
 		begin_offset = end_offset;
 	}
 
-	for(i = 0; i < number_threads; ++i)
+    for(i = 0; i < cur_pthread; ++i) {
 		pthread_join(pthreads[i], NULL);
+		free(arg[i]);
+	}
+
 	free(pthreads);
-	
+	free(arg);
 
-	for(i = 0; i < list->current_length; ++i)
-		free(list->head + i);
-	list_destroy(list);
-	free(list);
+    return 0;
 }
-
